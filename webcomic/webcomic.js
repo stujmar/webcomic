638be3e4954a9bc4510b5c31e72dd2de1e9faa3d
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-24 16:39:29 -0600

    brought in code to render react in the root

---
+++ webcomic/webcomic.js
@@ -0,0 +1,9 @@
+const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+
+const App = () => (
+       <div>
+           this is coming from the js file.
+       </div>
+    )
+   ReactDOM.render(<App />, document.querySelector('#root'));
